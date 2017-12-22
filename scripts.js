76f97b6189c6476b3b497087f17cf70b15445c11
Kamil Stangret <kamilstangret@wp.pl>
2017-12-22 20:28:09 +0100

    Add scripts.js with content

---
+++ scripts.js
@@ -0,0 +1,9 @@
+// scripts.js
+
+/* 
+var age = prompt('How old are you?');
+alert('I am , ' + age + ' years old');
+console.log('I am, ' + age + ' years old'); 
+*/
+
+// It is not allowed to use this program if you are under 18 years old
